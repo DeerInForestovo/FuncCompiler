1e412d46d53482f310a6441bac9a01322db63695
sjkmost <sjkmoster@outlook.com>
2024-11-02 17:58:00 +0800

    Do block display

--- src/ast.cpp
+++ src/ast.cpp
@@ -159,6 +159,33 @@ void ast_index::display(int tabs) const {
     std::cout << std::endl;
 }
 
+void action_exec::display(int tabs) const {
+    display_tabs(tabs);
+    display_structure("EXEC: ");
+    std::cout << std::endl;
+
+    body->display(tabs + 1);
+}
+
+void action_return::display(int tabs) const {
+    display_tabs(tabs);
+    display_structure("RETURN: ");
+    std::cout << std::endl;
+
+    body->display(tabs + 1);
+    std::cout << std::endl;
+}
+
+void action_bind::display(int tabs) const {
+    display_tabs(tabs);
+    display_structure("BIND ");
+    display_function_name(name);
+    std::cout << ' ';
+
+    act->display(tabs);
+    std::cout << std::endl;
+}
+
 void pattern_var::display() const {
     display_structure("PATTERN_VAR: ");
     std::cout << var;
@@ -181,6 +208,18 @@ void definition_defn::display(int tabs) const {
 
     body->display(tabs + 1);
     std::cout << std::endl;
+}
+
+void definition_defn_action::display(int tabs) const {
+    display_tabs(tabs);
+    display_structure("DEFN: ");
+    display_function_name(name);
+    for (const auto& param: params)
+        std::cout << ' ' << param;
+    std::cout << std::endl;
+
+    for (const auto& action: body)
+        action->display(tabs + 1);
 }
 
 void definition_data::display(int tabs) const {
