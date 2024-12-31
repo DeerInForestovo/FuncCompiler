b6e442a81f77256686e0423763c7946eaaf40c8a
sjkmost <sjkmoster@outlook.com>
2024-12-31 20:37:16 +0800

    fix a typo

--- src/type_env.hpp
+++ src/type_env.hpp
@@ -9,19 +9,15 @@ using type_env_ptr = std::shared_ptr<type_env>;
 struct type_env {
     type_env_ptr parent;
     std::map<std::string, type_scheme_ptr> names;
-    std::map<std::string, type_scheme_ptr> private_names;
     std::map<std::string, type_ptr> type_names;
 
     type_env(type_env_ptr p) : parent(std::move(p)) {}
     type_env() : type_env(nullptr) {}
 
     type_scheme_ptr lookup(const std::string& name) const;
-    type_scheme_ptr lookup_private(const std::string& name) const;
     type_ptr lookup_type(const std::string& name) const;
     void bind(const std::string& name, type_ptr t);
     void bind(const std::string& name, type_scheme_ptr t);
-    void bind_private(const std::string& name, type_ptr t);
-    void bind_private(const std::string& name, type_scheme_ptr t);
     void bind_type(const std::string& type_name, type_ptr t);
     void generalize(const std::string& name, type_mgr& mgr);
 };
