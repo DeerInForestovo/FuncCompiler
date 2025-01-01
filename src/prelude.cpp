bfa9acda11a3123e2b7189d6a420757fee94f8c6
sjkmost <sjkmoster@outlook.com>
2025-01-01 13:20:12 +0800

    complete print

--- src/prelude.cpp
+++ src/prelude.cpp
@@ -17,10 +17,7 @@ void generate_read_llvm(llvm_context &ctx) {
     );
     FunctionCallee isspace_func = ctx.module.getOrInsertFunction("isspace", isspace_type);
     Value *isspace_call = ctx.builder.CreateCall(isspace_func, { getchar_call });
-    Value *sign = ctx.builder.CreateICmpNE(
-        isspace_call, 
-        ConstantInt::get(ctx.builder.getInt32Ty(), 0)
-    );
+    Value *sign = ctx.builder.CreateICmpNE(isspace_call, ConstantInt::get(ctx.builder.getInt32Ty(), 0));
 
     Value *ret_char = ctx.builder.CreateTrunc(getchar_call, ctx.builder.getInt8Ty());
 
@@ -54,4 +51,50 @@ void generate_read_llvm(llvm_context &ctx) {
 }
 
 void generate_print_llvm(llvm_context &ctx) {
+    Function *f = ctx.create_custom_function("print", 1);
+    ctx.builder.SetInsertPoint(&f->getEntryBlock());
+
+    FunctionType *putchar_type = FunctionType::get(ctx.builder.getInt32Ty(), { ctx.builder.getInt32Ty() }, false);
+    FunctionCallee putchar_func = ctx.module.getOrInsertFunction("putchar", putchar_type);
+
+    ctx.create_unwind(f);
+
+    Value *top_node = ctx.create_peek(f, ctx.create_size(0));
+    Value *tag_i8 = ctx.unwrap_data_tag(top_node);
+    Value *tag_i1 = ctx.builder.CreateICmpNE(tag_i8, ConstantInt::get(ctx.builder.getInt8Ty(), 0));
+
+    BasicBlock *safety_block = BasicBlock::Create(ctx.ctx, "safety", f);
+    BasicBlock *cons_block = BasicBlock::Create(ctx.ctx, "consBlock", f);
+    BasicBlock *nil_block = BasicBlock::Create(ctx.ctx, "nilBlock", f);
+
+    ctx.builder.CreateCondBr(tag_i1, cons_block, nil_block);
+
+    ctx.builder.SetInsertPoint(cons_block);
+    ctx.create_split(f, ctx.create_size(2));
+    ctx.create_unwind(f);
+
+    Value *top_char_node = ctx.create_pop(f);
+    Value *char_val_i8 = ctx.unwrap_data_tag(top_char_node);
+    Value *char_val_i32 = ctx.builder.CreateZExt(char_val_i8, ctx.builder.getInt32Ty());
+    ctx.builder.CreateCall(putchar_func, { char_val_i32 });
+
+    Value *recur_print = ctx.create_global(f, f, ctx.create_i32(1));
+    Value *n_xs = ctx.create_pop(f);
+    ctx.create_push(f, ctx.create_app(f, recur_print, n_xs));
+    ctx.create_unwind(f);
+
+    ctx.builder.CreateBr(safety_block);
+
+    ctx.builder.SetInsertPoint(nil_block);
+
+    Value *newline_val = ctx.builder.getInt32('\n');
+    ctx.builder.CreateCall(putchar_func, { newline_val });
+    
+    ctx.builder.CreateBr(safety_block);
+
+    ctx.builder.SetInsertPoint(safety_block);
+
+    ctx.create_update(f, ctx.create_size(0));
+
+    ctx.builder.CreateRetVoid();
 }
